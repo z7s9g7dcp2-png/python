2e74ac8ae96ca33252648b0c867ffe7f264d38b3
Leylahni <leylahniyasmin@icloud.com>
2026-02-05 22:51:38 +0300

    Updated python

---
+++ assignment2.py
@@ -0,0 +1,9 @@
+#Create a tuple of 8 different planets
+#Tuple()
+
+planets = ("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+#print all the planets
+print(planets)
+
+#print from index 2 to 6
+print(planets[2:7])
